fa8c21a11141a7e315470d55c54eeba0f7f77941
KNMoise <obiomoise@gmail.com>
2023-10-27 16:33:43 +0200

    start on loops

--- 005_loops.php
+++ 005_loops.php
@@ -1,5 +1,38 @@
 <?php
 // --------------------------------loops----------------------------
+/*
+-------------------for loops syntax----------------------
+for (initialize, conditions, increments){
+    //code to be executed in each iteration of the loop
+}
+*/
+
+// for($x = 0; $x <= 10; $x ++){
+//     // echo "The value of x is: ". $x ."<br>";
+//     echo $x;
+// };
+// // ------------------------While loop syntax-------------------
+// while(condition){
+//     code to be executed
+// }
+
+// $x = 1;
+
+// while($x <= 15){
+//     echo "the value of x is :" . $x . "<br>";
+//     $x = $x + 1;
+//     // echo $x;
+// }
+
+// ------------------do while loop--------------------------------
+
+$X = 1;
+
+do {
+    echo " Number of x is:" .$X . "<br>";
+    $X++;
+}
+while ( $X <= 5);
 
 
 
